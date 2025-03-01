338895721ceb0e669ea1185fb91dafc0b0728a3e
himanshuu004 <himanshuu004@gmail.com>
2025-03-02 03:22:14 +0530

    new

--- index.js
+++ index.js
@@ -8,7 +8,38 @@ app.use(express.urlencoded({ extended: true}));
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"views"));
 
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
+
+let  posts = [
+    {
+        username: "Himanshu Rawat",
+        content: "I love Music",
+    },
+    {
+        username: "Himanshu Rawat",
+        content: "I love Music",
+    },
+    {
+        username: "Himanshu Rawat",
+        content: "I love Music",
+    }
+];
+
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
+});
+
+
+app.get("/posts/new",(req,res)=>{
+    res.render("form.ejs");
+})
+
+app.post("/post",(req,res)=>{
+    let {username , content} = req.body;
+    posts.push({username,content});
+    res.redirect("/posts");
+})
+
  
 app.get("/",(req,res)=>{
     res.send("Server workin well");
